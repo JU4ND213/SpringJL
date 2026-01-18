776bde7257d43099ee33128c1ab528f4f6d91182
bfabian333 <63011768+bfabian333@users.noreply.github.com>
2026-01-18 18:33:44 -0500

    :Domingo18:

---
+++ src/main/java/com/jl/matriculas/servicio/IProfesorServicio.java
@@ -0,0 +1,14 @@
+package com.jl.matriculas.servicio;
+
+import java.util.List;
+
+import com.jl.matriculas.modelo.entidad.Profesor;
+
+public interface IProfesorServicio {
+
+	public Profesor insertarProfesor(Profesor nuevoProfesor);
+	public Profesor editarProfesor(int idProfesor);
+	public void EliminarProfesor(int idProfesor);
+	public List<Profesor> listarProfesor();
+
+}
---
+++ src/main/java/com/jl/matriculas/servicio/impl/ProfesorImpl.java
@@ -0,0 +1,37 @@
+package com.jl.matriculas.servicio.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import com.jl.matriculas.servicio.IProfesorServicio;
+import com.jl.matriculas.modelo.entidad.Profesor;
+import com.jl.matriculas.repositorio.IProfesorRepositorio;
+@Service
+@Component
+public class ProfesorImpl implements IProfesorServicio {
+	@Autowired
+	public IProfesorRepositorio repositorioProfesor;
+	@Override
+	public Profesor insertarProfesor(Profesor nuevoProfesor) {
+		// TODO Auto-generated method stub
+		return repositorioProfesor.save(nuevoProfesor);
+}
+	@Override
+	public Profesor editarProfesor(int idProfesor) {
+		// TODO Auto-generated method stub
+		return repositorioProfesor.findById(idProfesor).get();
+	}
+	@Override
+	public void EliminarProfesor(int idProfesor) {
+		// TODO Auto-generated method stub
+		repositorioProfesor.delete(editarProfesor(idProfesor));
+	}
+	@Override
+	public List<Profesor> listarProfesor() {
+		// TODO Auto-generated method stub
+		return repositorioProfesor.findAll();
+	}
+}
